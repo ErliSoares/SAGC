1b950e4a51c41ecfdbef205c8f68c36c7149c568
Erli Silva Soares <erli-silva@hotmail.com>
2012-09-19 18:54:07 -0300

    Criação dos formularios

---
+++ SAGC/FrmFuncionario.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmFuncionario
+
+End Class
---
+++ SAGC/FrmPrincipal.vb
@@ -0,0 +1,6 @@
+﻿Public Class FrmPrincipal
+
+    Private Sub FuncionárioToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FuncionárioToolStripMenuItem.Click
+        FrmFuncionario.ShowDialog()
+    End Sub
+End Class
